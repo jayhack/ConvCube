b0b1ca8d66793ba35f60e339a200c48edb48b005
Jay Hack <jhack@stanford.edu>
2015-02-27 08:42:57 -0800

    not able to insert currently... getting duplicate key errors

--- record.py
+++ record.py
@@ -18,13 +18,46 @@ Winter 2015
 jhack@stanford.edu
 ##################
 """
+import time
+import numpy as np
 import cv2
-from ModalDB import ModalClient
+from ModalDB import ModalClient, Video, Frame
 
 from schema import convcube_schema
 
 if __name__ == '__main__':
 
 	#=====[ Step 1: boot up	and create convcube schema ]=====
-	modal_client = ModalClient('./data/db', schema=convcube_schema)
+	client = ModalClient('./data/db', schema=convcube_schema)
+
+	#=====[ Step 2: turn on the webcam	]=====
+	cap = cv2.VideoCapture(0)
+	cv2.namedWindow('DISPLAY')
+
+	#=====[ Step 3: create video name	]=====
+	video_name = 'video_' + str(time.time()).split('.')[0]
+	video = client.insert(Video, video_name, {})
+	print 'Video name: %s' % video_name
+
+	#=====[ Step 3: capture frames until ESC	]=====
+	i = 0
+	while True:
+
+		#####[ GRAB IMAGE FROM WEBCAM	]#####
+		ret, image = cap.read()
+		cv2.imshow('DISPLAY', image)
+
+		#####[ INSERT INTO DB	]#####
+		frame_name = 'frame_' + str(i)
+		frame = client.insert(Frame, frame_name, {}, parent=video, method='mv')
+		frame['image'] = image
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	#=====[ Step 4: cleanup	]=====
+	cap.release()
+	cv2.destroyAllWindows()
+
+
 
