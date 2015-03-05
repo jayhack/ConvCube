1065c0aeba63b92ee602537f8aa1e366d823cb7e
Jay Hack <jhack@stanford.edu>
2015-03-05 00:53:16 -0800

    added init to wrangling

---
+++ convcube/utils/__init__.py
@@ -0,0 +1,11 @@
+__all__ = 	[
+				#=====[ wrangling	]=====
+				'has_label', 'iter_labeled_frames', 'iter_unlabeled_frames'
+
+				#=====[ drawing	]=====
+				'draw_points'
+			]
+
+from wrangling import has_label
+from wrangling import iter_labeled_frames
+from wrangling import iter_unlabeled_frames
--- convcube/utils/wrangling.py
+++ convcube/utils/wrangling.py
@@ -1,10 +1,6 @@
-import pickle
-import random
-import numpy as np
 from ModalDB import ModalClient
 from ModalDB import Video
 from ModalDB import Frame
-from preprocess import image2localization_array
 
 
 ################################################################################
@@ -29,35 +25,6 @@ def iter_unlabeled_frames(client):
 	for frame in client.iter(Frame):
 		if not has_label(frame):
 			yield frame
-
-
-
-
-
-################################################################################
-####################[ LABEL FACTORY ]###########################################
-################################################################################
-
-
-def get_label_heatmap(image, kpts):
-	"""(image, kpts) -> output of map
-
-		TODO: this needs to be a 1d array...
-	"""
-	raise NotImplementedError
-	kpts = np.array(kpts).astype(np.uint16)
-	label_image = np.zeros((image.shape[0], image.shape[1]))
-	label_image[kpts[:,1], kpts[:,0]] = 1
-	label_image = cv2.GaussianBlur(label_image, (5,5), 2)
-	return label_image
-
-
-def get_convnet_inputs(frame):
-	"""ModalDB.Frame -> (X, y_localization, y_heatmap)"""
-	X = image2convnet_array(frame['image'])
-	y_localization = get_label_localization(frame['interior_points'])
-	y_heatmap = get_label_heatmap(frame['image'])
-	return X, y_localization, y_heatmap
 
 
 
