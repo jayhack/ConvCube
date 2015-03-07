d4ff35e73f0d4c06bb14bd0ac29ed6eb76474cfa
Jay Hack <jhack@stanford.edu>
2015-03-07 08:15:53 -0800

    localization is basically working; just gotta fuck with some parameters. marking more data now

--- convcube/localization/convnet.py
+++ convcube/localization/convnet.py
@@ -266,7 +266,7 @@ class LocalizationConvNet(object):
 																		model, self.localization_convnet, 
 																		dropout=None, reg=0.00, 
 																		learning_rate=0.0002, 
-																		batch_size=50, num_epochs=15,
+																		batch_size=50, num_epochs=30,
 																		learning_rate_decay=0.95,
 																		update='rmsprop', verbose=True
 																	)
